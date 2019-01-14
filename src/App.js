2d8ee17b94ba3897ffb0357d363daea7b8f8c72c
Nataliya Shipelova <natulechka_m.25@mail.ru>
2019-01-14 21:23:45 +0300

    feat (chat) add initial app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
