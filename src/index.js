b085b088a02a8e95e37056932c00cd31fa8b4c61
Nataliya Shipelova <natulechka_m.25@mail.ru>
2019-01-16 23:45:35 +0300

    feat (chat) add scripts to start with single command
    add nodemon to start server side and concurrently to start two scripts

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
-import createSagaMiddleware from "redux-saga";
+import createSagaMiddleware from 'redux-saga';
 import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import App from './App';
 import * as reducers from "./store/reducers";
