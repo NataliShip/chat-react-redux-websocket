ccfad50d0dad67ef32a950302afc6c6b1ed0615d
Nataliya Shipelova <natulechka_m.25@mail.ru>
2019-01-16 17:17:25 +0300

    feat (chat) write server side

--- server/app.js
+++ server/app.js
@@ -1,23 +1,60 @@
-const WebSocket = require('ws'); // use library web sockets
+const WebSocket = require('ws');
 
 // create server on port 8989
-const wss = new WebSocket.Server({ port: 8989 });
+const wss = new WebSocket.Server({
+  port: 8989
+});
 
 const users = [];
 
+// send data to clients
 const broadcast = (data, ws) => {
   wss.clients.forEach(client => {
+    // send for everyone except author
     if (client.readyState === WebSocket.OPEN && client !== ws) {
       client.send(JSON.stringify(data))
     }
   })
 }
 
-// callback for server
+// callbacks for server
 wss.on('connection', ws => {
   let index;
-  console.log('connect');
+  console.log('new user connected');
   ws.on('message', message => {
+    const data = JSON.parse(message);
+    switch (data.type) {
+      case 'ADD_USER': {
+        index = users.length;
+        users.push({ name: data.name, id: index + 1});
+        ws.send(JSON.stringify({
+          type: 'USERS_LIST',
+          users
+        }))
+        broadcast({
+          type: 'USERS_LIST',
+          users
+        }, ws)
+        break
+      }
+      case 'ADD_MESSAGE':
+        broadcast({
+          type: 'ADD_MESSAGE',
+          message: data.message,
+          author: data.author
+        }, ws)
+        break
+      default:
+        break
+    }
+  })
 
+  ws.on('close', () => {
+    console.log('user disconnect');
+    users.splice(index, 1);
+    broadcast({
+      type: 'USERS_LIST',
+      users
+    }, ws)
   })
 })
