b93e0bca1d700fe6a31e397fada51f13de6b121c
Nataliya Shipelova <natulechka_m.25@mail.ru>
2019-01-16 11:18:46 +0300

    feat (chat) style login form and add validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
-const Login = (props) => {
-  return (
-    <div className='login'>
-      <span className='login__header'>Для входа в чат введите имя</span>
-      <input
-        onKeyPress={(e) => {
-          if (e.key === 'Enter') {
-            props.addUser(e.currentTarget.value)
-            e.currentTarget.value = ''
-          }
-        }}
-        className='login__input'
-        placeholder='Имя'
-        type="text"
-      />
-    </div>
-  )
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showLoginForm: true,
+      showError: false
+    }
+  }
+
+  login = (e) => {
+    if (e.key === 'Enter') {
+      this.setState(this.nameInput.value ? {showLoginForm: false} : {showError: true});
+      if (this.nameInput.value) {
+        this.props.addUser(this.nameInput.value);
+      }
+    }
+  }
+
+  render() {
+    const {state: {showLoginForm, showError}} = this
+
+    return (
+      <div>
+        {showLoginForm
+          ? <div className='login'>
+            <span className='login__header'>Для входа в чат введите имя</span>
+            <input
+              onKeyPress={this.login}
+              className='login__input'
+              placeholder='Имя'
+              type="text"
+              ref = {(node) => {this.nameInput = node}}
+            />
+            {showError
+              ? <span className='login__error'>Вы забыли ввести имя</span>
+              : null
+            }
+
+          </div>
+          : null
+        }
+      </div>
+    )
+  }
 }
 
 Login.propTypes = {
   addUser: PropTypes.func.isRequired
 }
+
 export default Login;
